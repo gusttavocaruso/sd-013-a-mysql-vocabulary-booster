93d492f0304a63812e321dc03e0596a84677961b
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-10-23 16:57:52 -0300

    Req 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN JOB_NAME VARCHAR(100))
+BEGIN
+		SELECT ROUND(AVG(SALARY), 2) AS "Média salarial"
+    FROM employees
+    WHERE JOB_ID = (SELECT JOB_ID FROM jobs WHERE JOB_TITLE = JOB_NAME);
+END $$
+
+DELIMITER ;
