d4f032b4ed3a03862e14a76457fcb4b0262c84c3
Murilo Rainho <murilorainho01@gmail.com>
2021-10-24 20:30:39 -0300

    realizando requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,19 @@
+USE hr;
+
+DELIMITER $$;
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(employeeEmail VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE result INT;
+  SELECT COUNT(jh.EMPLOYEE_ID)
+  FROM hr.job_history jh
+  WHERE jh.EMPLOYEE_ID = (
+    SELECT e.EMPLOYEE_ID
+	FROM hr.employees e
+    WHERE e.EMAIL = employeeEmail
+  ) INTO result;
+  RETURN result;
+END $$;
+
+DELIMITER ;
