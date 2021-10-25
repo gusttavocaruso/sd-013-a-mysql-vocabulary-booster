9eb00f88bd3b7abe61680188d1c2cf559fd081f9
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-10-25 17:00:16 -0300

    requirement 10 done

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+prod.ProductName AS Produto,
+MIN(ord.Quantity) AS Mínima,
+MAX(ord.Quantity) AS Máxima,
+ROUND(AVG(ord.Quantity), 2) AS Média
+FROM w3schools.order_details AS ord
+ INNER JOIN w3schools.products AS prod
+ ON ord.ProductID = prod.ProductID
+ GROUP BY ord.ProductID
+ HAVING AVG(ord.Quantity) > 20.00
+ ORDER BY AVG(ord.Quantity) ASC, prod.ProductName ASC;
