90bc6ad780f30e0b2e5d79e1e7f7f8731204c56c
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-25 18:41:31 -0300

    challenge 3 adjustments

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,6 @@
-SELECT JOB_TITLE AS Cargo, CONVERT(MAX_SALARY - MIN_SALARY, DOUBLE) AS "Diferença entre salários máximo e mínimo"
-FROM hr.jobs
-ORDER BY CONVERT(MAX_SALARY - MIN_SALARY, double) ASC, Cargo;
+SELECT
+    JOB_TITLE AS Cargo,
+    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+FROM
+    hr.jobs
+ORDER BY (MAX_SALARY - MIN_SALARY) , Cargo;
