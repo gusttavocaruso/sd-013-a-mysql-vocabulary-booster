d469b1eb3d3dc7cc667bddc6520dc3bf501c3587
turtore <brunosap@gmail.com>
2021-10-25 17:49:22 -0300

    atende requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT contactName AS 'Nome de contato',
+shippername AS 'Empresa que fez o envio',
+orderdate AS 'Data do pedido'
+
+
+FROM w3schools.customers AS c
+
+INNER JOIN w3schools.orders AS o
+ON c.customerid = o.customerid
+
+INNER JOIN w3schools.shippers AS s
+ON o.shipperid = s.shipperid
+
+ORDER BY contactname, shippername, orderdate;
