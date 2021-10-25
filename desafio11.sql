8224f601693023c7de244b72351b7acebb70b66e
Icaro Ferreira <icaro.iky@gmail.com>
2021-10-25 19:13:10 -0300

    Req 11 e 12

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+t1.ContactName AS 'Nome',
+t1.Country AS 'País',
+COUNT(*) AS 'Número de compatriotas'
+FROM
+w3schools.customers AS t1, w3schools.customers AS t2
+WHERE t1.Country = t2.Country AND t1.CustomerName <> t2.CustomerName
+GROUP BY t1.CustomerName
+ORDER BY t1.ContactName;
