3a2c8afe1a2c009075564c744d97b01fc21dbf68
LuizFJP <luizfjportela@gmail.com>
2021-10-25 19:29:38 -0300

    requirement 02, 03, 04, 05

---
+++ desafio4.sql
@@ -0,0 +1,19 @@
+SELECT
+	j.JOB_TITLE AS Cargo,
+	ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
+    CASE
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN "Júnior"
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN "Pleno"
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 7501 AND 10500 THEN "Sênior"
+    WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN "CEO" 
+    ELSE 'Estagiário'
+    END AS Senioridade
+FROM
+	employees e
+INNER JOIN
+	jobs j
+ON
+	e.JOB_ID = j.JOB_ID
+GROUP BY Cargo
+ORDER BY `Média Salarial` ASC,
+Cargo ASC;
