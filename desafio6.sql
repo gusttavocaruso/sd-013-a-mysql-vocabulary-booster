3707103e1c9873cac1463af8e2171406980cedad
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-25 18:43:44 -0300

    requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -18,3 +18,18 @@
 			ON (condição_3)
 	ORDER BY (coluna_1) DESC, (coluna_2);
 */
+
+SELECT 
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+  J.JOB_TITLE AS 'Cargo',
+  JH.START_DATE AS 'Data de início do cargo',
+  D.DEPARTMENT_NAME AS 'Departamento'
+FROM
+  hr.employees AS E
+  INNER JOIN hr.job_history AS JH
+    ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS J
+    ON JH.JOB_ID = JH.JOB_ID
+  INNER JOIN hr.departments AS D
+    ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) DESC, J.JOB_TITLE;
