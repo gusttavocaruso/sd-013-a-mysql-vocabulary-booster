0ce3426dcea5db73acd667ebb35074529c2f4659
Gustavo-Jezini <gustavojezini@gmail.com>
2021-10-25 15:35:39 -0300

    Enviando questoes, tentativa 9

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,5 @@ COUNT(*) AS 'Números de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
 AND t1.CustomerID <> t2.CustomerID
-GROUP BY t1.CustomerID
+GROUP BY t1.CustomerID, País
 ORDER BY t1.ContactName;
