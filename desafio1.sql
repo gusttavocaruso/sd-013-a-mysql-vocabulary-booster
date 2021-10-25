978bfa1292040010456be5d4f86446fd9dc1f62e
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-10-25 15:33:58 -0300

    Requisito 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
-SELECT COUNTRY_NAME, IF(REGION_ID = 2, 'incluído', 'não incluído') AS 'Europe'
+SELECT COUNTRY_NAME AS País, IF(REGION_ID = 2, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries 
 ORDER BY COUNTRY_NAME;
