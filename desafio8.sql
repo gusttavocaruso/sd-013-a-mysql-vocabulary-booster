872d3805381846d393e7b19e877cdc8ca6e8184b
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-10-25 20:40:44 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,17 +1,13 @@
 SELECT 
     c.ContactName AS 'Nome de contato',
-    (SELECT 
-            ShipperName
-        FROM
-            w3schools.shippers
-        WHERE
-            ShipperID = o.ShipperID
-        LIMIT 1) AS 'Empresa que fez o envio',
+    s.ShipperName AS 'Empresa que fez o envio',
     o.OrderDate AS 'Data do pedido'
 FROM
     w3schools.orders AS o
         INNER JOIN
     w3schools.customers AS c ON o.CustomerID = c.CustomerID
-    WHERE
-		o.ShipperID IN (1,2)
-    ORDER BY c.ContactName asc;
+        INNER JOIN
+    w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE
+    o.ShipperID IN (1 , 2)
+ORDER BY c.ContactName ASC , s.ShipperName ASC , o.OrderDate ASC;
