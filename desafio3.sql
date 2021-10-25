5dfa7e4bd375cd6037fabae9386c40915d840f3d
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-25 16:03:57 -0300

    requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,19 @@
 -- 3 - Exiba os cargos com a diferença entre seus salários máximo e mínimo
+
+/*
+1. Estrutura básica da query:
+
+	SELECT
+		JOB_TITLE AS (coluna 1),
+        (MAX_SALARY - MIN_SALARY) AS (coluna 2)
+	FROM hr.jobs
+	ORDER BY (coluna 2), (coluna 1) ASC;
+*/
+
+-- 2. Substituindo com as informações extraidas do enunciado:
+
+SELECT
+	JOB_TITLE AS 'Cargo',
+	MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, `Cargo` ASC;
