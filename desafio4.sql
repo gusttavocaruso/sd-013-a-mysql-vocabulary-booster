4333e2b1eba0b143acbe180e3bcb0113dfcf8cb6
marcos <marc0sdurval20@gmail.com>
2021-10-25 17:32:57 -0300

    desafio02

--- desafio4.sql
+++ desafio4.sql
@@ -2,19 +2,17 @@ SELECT
     (SELECT 
             JOB_TITLE
         FROM
-            hr.jobs as N
+            hr.jobs AS N
         WHERE
-            N.JOB_ID = E.JOB_ID) as Cargo,
-	round(AVG(SALARY),2) AS "Média Salarial",
-    CASE 
-    WHEN round(AVG(SALARY),2) BETWEEN 2000 AND 5800 THEN "Júnior"
-	  WHEN round(AVG(SALARY),2) BETWEEN 5801 AND 7500 THEN "Pleno"
-    WHEN round(AVG(SALARY),2) BETWEEN 7501 AND 10500 THEN "Sênior"
-	ELSE 'CEO'
-    END
-    AS 'Nível'
+            N.JOB_ID = E.JOB_ID) AS Cargo,
+    ROUND(AVG(SALARY), 2) AS 'Média Salarial',
+    CASE
+        WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Nível'
 FROM
-    hr.employees as E
+    hr.employees AS E
 GROUP BY JOB_ID
-ORDER BY `Média Salarial` ASC, Cargo ASC;
-
+ORDER BY `Média Salarial` ASC , Cargo ASC;
