9fa59ea3a0a9fc3aed8fbdcc64ca9e1e16de1ac9
Walace Nascimento <wlacens@hotmail.com>
2021-10-25 16:28:52 -0300

    Cria os arquivo dos desafios e resolve o desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,7 @@
+-- Exiba os países
+-- e indicando se cada um deles se encontra ou não na região formada pela Europa
+
+SELECT COUNTRY_NAME AS Pais,
+IF (REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão'
+FROM hr.countries;
+
