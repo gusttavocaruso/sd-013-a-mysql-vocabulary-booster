a23ecf35b736a401bfc1c6f77f9f4ab5d6d8fd46
Gustavo Mauricio <gmauricio207@gmail.com>
2021-10-25 19:00:29 -0300

    Req 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,13 @@
 --  10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
 SELECT 
     p.ProductName AS Produto,
-    MIN(od.QUANTITY) AS Mínima,
-    MAX(od.Quantity) AS Máxima,
-    ROUND(AVG(od.Quantity), 2) AS Média
+    MIN(od.QUANTITY) AS 'Mínima',
+    MAX(od.Quantity) AS 'Máxima',
+    ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM
     products AS p
         INNER JOIN
     order_details AS od ON p.Productid = od.ProductID
 GROUP BY p.ProductName
-ORDER BY Média ASC , Produto ASC;
+HAVING ROUND(AVG(od.Quantity), 2) > 20
+ORDER BY ROUND(AVG(od.Quantity), 2) ASC , p.ProductName ASC;
