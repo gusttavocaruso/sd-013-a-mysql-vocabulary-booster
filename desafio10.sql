dc55f9d163339f04f43d4aec0d93a90da21e495e
Thiago <thiagoc.carboneri@gmail.com>
2021-10-25 18:55:12 -0300

    requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,10 @@
-SELECT p.ProductName AS 'Produto',
+ SELECT p.ProductName AS 'Produto',
   MIN(od.Quantity) AS 'Mínima',
   MAX(od.Quantity) AS 'Máxima',
   ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM products p
   INNER JOIN order_details od ON od.ProductID = p.ProductID
 GROUP BY p.ProductName
+HAVING ROUND(AVG(od.Quantity), 2) > 20
 ORDER BY ROUND(AVG(od.Quantity), 2) ASC,
   p.ProductName ASC;
