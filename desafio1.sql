fcc00d84c969fea2a6ca96358ab9c936ac32b7c9
Wesley Maia <maia.weol@gmail.com>
2021-10-25 15:55:38 -0300

    Lint

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 -- SELECT * FROM hr.countries;
 SELECT 
-	C.COUNTRY_NAME AS 'País', 
-    IF (R.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS `Status Inclusão`
+C.COUNTRY_NAME AS 'País', 
+IF (R.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries AS C
 INNER JOIN hr.regions AS R
 ON (C.REGION_ID = R.REGION_ID)
