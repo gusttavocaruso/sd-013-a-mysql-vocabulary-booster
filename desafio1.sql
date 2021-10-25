4f3f4913eaa512dcd1562285b85309d31de54b5e
Walace Nascimento <wlacens@hotmail.com>
2021-10-25 17:24:31 -0300

    Resolve o requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,4 @@
--- Exiba os países
--- e indicando se cada um deles se encontra ou não na região formada pela Europa
-
-SELECT COUNTRY_NAME AS Pais,
-IF (REGION_ID = 1, 'incluido', 'não incluido') AS 'Status Inclusão'
-FROM hr.countries;
-
+SELECT COUNTRY_NAME AS País,
+IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries  
+ORDER BY COUNTRY_NAME;
