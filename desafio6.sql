c1faf942d0a88bd7f0218f5969950ad4673a0b4d
Thiago <thiagoc.carboneri@gmail.com>
2021-10-25 16:50:40 -0300

    requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,13 @@
+USE hr;
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
   j.JOB_TITLE AS 'Cargo',
-  e.HIRE_DATE AS 'Data de início do cargo',
+  jh.START_DATE AS 'Data de início do cargo',
   d.DEPARTMENT_NAME AS 'Departamento'
 FROM employees e
-  INNER JOIN jobs j ON j.JOB_ID = e.JOB_ID
-  INNER JOIN departments d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+INNER JOIN jobs j ON j.JOB_ID = e.JOB_ID
+  INNER JOIN job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+  INNER JOIN departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC,
   j.JOB_TITLE ASC;
+  SELECT * FROM job_history;
+
