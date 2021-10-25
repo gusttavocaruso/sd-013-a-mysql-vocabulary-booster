e8e97a0e07d53d5cab22a7dc5b6c3ad2eb140f24
Rafhael Gomes <rafhael@protonmail.com>
2021-10-25 16:21:07 -0300

    desafio 6 e 7

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    j.JOB_TITLE AS Cargo,
+    jh.START_DATE AS 'Data de início do cargo',
+    d.DEPARTMENT_NAME
+FROM
+    hr.job_history jh
+        INNER JOIN
+    hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+        INNER JOIN
+    hr.departments d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+        INNER JOIN
+    hr.jobs j ON jh.JOB_ID = j.JOB_ID
+ORDER BY `Nome completo` DESC , Cargo ASC
---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT 
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de ínicio',
+    e.SALARY AS Salário
+FROM
+    hr.job_history jh
+        INNER JOIN
+    hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+    MONTH(jh.START_DATE) = 1
+        OR MONTH(jh.START_DATE) = 2
+        OR MONTH(jh.START_DATE) = 3
+ORDER BY `Nome completo` ASC , jh.START_DATE ASC;
