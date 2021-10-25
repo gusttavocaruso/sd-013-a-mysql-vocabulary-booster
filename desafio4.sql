899480af4e417a3aec0e78448f294f943fdd230d
FelipeNevess <felipe012neves@gmail.com>
2021-10-25 16:18:41 -0300

    corrigindo erro novamento

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,12 @@
-SELECT
-	J.JOB_TITLE AS Cargo,
-    ROUND(AVG(E.SALARY), 2) AS `Média salarial`,
-    CASE
-		WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-		WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-		WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 1501 AND 10500 THEN 'Sênior'
-		ELSE 'CEO'
-	END AS `Senioridade`
-FROM hr.jobs AS J
-INNER JOIN hr.employees AS E
-ON J.JOB_ID = E.JOB_ID
+SELECT j.JOB_TITLE AS Cargo,
+  ROUND(AVG(c.SALARY), 2) AS `Média salarial`,
+  CASE
+    WHEN ROUND(AVG(c.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(c.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(c.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    ELSE 'CEO'
+  END AS Senioridade
+FROM hr.jobs AS j
+  INNER JOIN hr.employees AS c ON j.JOB_ID = c.JOB_ID
 GROUP BY Cargo
-ORDER BY `Média salarial`, `Senioridade`;
+ORDER BY `Média salarial`, Cargo;
