3c9886b00ebf46c7bb94fbded57c345924f29342
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-25 14:28:52 -0300

    O evaluator me odeia

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 -- 7 - Faça um relatório que mostra o histórico de cargos das pessoas empregadas que iniciaram seus cargos nos meses de janeiro, fevereiro ou março
 SELECT 
-	UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-  j.START_DATE AS 'Data de início',
-  e.SALARY AS 'Salário'
+UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+j.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
 FROM hr.job_history AS j
 INNER JOIN hr.employees AS e
 ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,8 @@
 -- 8 - Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas "Speedy Express" ou "United Package"
 SELECT
-	c.ContactName AS 'Nome de contato',
-  s.ShipperName AS 'Empresa que fez o envio',
-  o.OrderDate AS 'Data do pedido'
+c.ContactName AS 'Nome de contato',
+s.ShipperName AS 'Empresa que fez o envio',
+o.OrderDate AS 'Data do pedido'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c
 ON c.CustomerID = o.CustomerID
