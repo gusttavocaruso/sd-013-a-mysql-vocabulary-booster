ba231f755083db5f27487ae2f46351be1d558081
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-25 18:33:34 -0300

    remove tabs e linha extra

--- desafio1.sql
+++ desafio1.sql
@@ -24,12 +24,12 @@
 
 -- 3. Por último, especificando a condição do valor da coluna `Status Inclusão`:
 SELECT
-	C.COUNTRY_NAME AS País,
-	CASE R.REGION_NAME
-		WHEN 'Europe' THEN 'incluído'
+  C.COUNTRY_NAME AS País,
+  CASE R.REGION_NAME
+    WHEN 'Europe' THEN 'incluído'
         ELSE 'não incluído'
     END AS `Status Inclusão`
 FROM hr.countries AS C
-	INNER JOIN hr.regions AS R
-	ON C.region_id = R.region_id
+  INNER JOIN hr.regions AS R
+  ON C.region_id = R.region_id
 ORDER BY COUNTRY_NAME;
--- desafio6.sql
+++ desafio6.sql
@@ -1,1 +1,20 @@
 -- 6 - Faça um relatório que mostra o histórico de cargos das pessoas empregadas
+
+/*
+1. Estrutura básica da query:
+
+	SELECT 
+		(coluna_1) AS 'Nome completo',
+		(coluna_2) AS 'Cargo',
+		(coluna_3) AS 'Data de início do cargo',
+		(coluna_4) AS 'Departamento'
+	FROM
+		(tabela_1) AS __
+        INNER JOIN (tabela_2) AS __
+			ON (condição_1)
+		INNER JOIN (tabela_3) AS __
+			ON (condição_2)
+		INNER JOIN (tabela_4) AS __
+			ON (condição_3)
+	ORDER BY (coluna_1) DESC, (coluna_2);
+*/
