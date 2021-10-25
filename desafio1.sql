1b714cb03def152360c920221b73eac9a2171c93
Anajulia-bs <juliehindius@gmail.com>
2021-10-25 16:52:42 -0300

    Desafio 1: resolvendo erros de interpretaçao

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,7 @@
-
-SELECT COUNTRY_NAME, REGION_ID,
-  CASE
-    WHEN REGION_ID = 1 THEN 'incluído'
+SELECT COUNTRY_NAME AS País,
+CASE
+WHEN REGION_ID = 1 THEN 'incluído'
 ELSE 'não incluído'
-END AS REGION_ID FROM hr.countries ORDER BY COUNTRY_NAME ASC;
+END AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY País;
