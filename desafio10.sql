35d0950a505d21e4c7e47e3877058b388c195033
Bruno Pinho <brunopinho@outlook.com>
2021-10-25 19:14:12 -0300

    feito requisitos 8, 9 e 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT 
+	p.ProductName AS `Produto`, 
+    MIN(od.quantity) AS `Mínima`, 
+    MAX(od.quantity) AS `Máxima`, 
+    ROUND(AVG(od.quantity), 2) AS `Média` 
+FROM 
+	w3schools.order_details od
+INNER JOIN w3schools.products p ON od.ProductID = p.ProductID
+GROUP BY od.ProductID
+HAVING AVG(od.quantity) > 20.00
+ORDER BY `Média`, `Produto`;
---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c.ContactName AS `Nome de contato`,
+    s.ShipperName AS `Empresa que faz o envio`,
+    o.OrderDate
+FROM
+    w3schools.orders o
+INNER JOIN 
+	w3schools.customers c ON o.CustomerID = c.CustomerID
+INNER JOIN
+	w3schools.shippers s ON o.ShipperID = s.ShipperID
+WHERE s.ShipperName IN('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que faz o envio`;
