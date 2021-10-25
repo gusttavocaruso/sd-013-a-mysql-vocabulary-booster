23425689df9698e48d8abef673a8568cece0c1b6
Gustavo-Jezini <gustavojezini@gmail.com>
2021-10-25 15:59:57 -0300

    ultima tentativa. Minha resposta esta identica ao teste e mesmo assim nao passa. Apaguei a rebaixe os bancos de dados

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS Cargo,
-e.HIRE_DATE AS 'Data de início do cargo',
+jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.jobs j
 INNER JOIN hr.employees e ON j.JOB_ID = e.JOB_ID
