0a37ad86f987f17912cf2d96e537f9801bcbb1e7
Gustavo Mauricio <gmauricio207@gmail.com>
2021-10-25 17:10:49 -0300

    Req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,15 @@
-SELECT * FROM employees;
-SELECT * FROM jobs;
-SELECT j.job_title AS Cargo, ROUND(AVG(e.SALARY),2) AS 'Média salarial' FROM hr.jobs AS j INNER JOIN employees AS e ON j.JOB_ID = e.JOB_ID
-GROUP BY  e.JOB_ID;
+SELECT 
+    j.job_title AS Cargo,
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN  ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN  ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN  ROUND(AVG(e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END Senioridade
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS j ON j.JOB_ID = e.job_id
+GROUP BY e.JOB_ID
+ORDER BY  'Média salarial' ASC;
