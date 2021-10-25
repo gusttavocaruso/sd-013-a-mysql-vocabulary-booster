bb5349c5c41f9a122099e574ef03a0c05a78f237
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-10-25 20:08:19 -0300

    requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT 
     UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-    e.HIRE_DATE AS 'Data de início',
+    h.START_DATE AS 'Data de início',
     e.SALARY AS 'Salário'
 FROM
     hr.employees AS e
@@ -8,4 +8,4 @@ FROM
     hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 WHERE
     MONTH(h.START_DATE) IN (1 , 2, 3)
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ASC
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ASC;
