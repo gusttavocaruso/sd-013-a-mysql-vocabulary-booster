6bf915874010f46c4cd4fe0762050958ade02fa6
Thiago <thiagoc.carboneri@gmail.com>
2021-10-25 16:31:34 -0300

    requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE,
+SELECT JOB_TITLE AS 'Cargo',
   (MAX_SALARY - MIN_SALARY) 'Variação Salarial',
   ROUND((MIN_SALARY / 12), 2) 'Média mínima mensal',
   ROUND((MAX_SALARY / 12), 2) 'Média máxima mensal'
