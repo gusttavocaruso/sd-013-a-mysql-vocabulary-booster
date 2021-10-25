de18f63bbe9599cd3de17cf52d62a716bf528e0b
Anajulia-bs <juliehindius@gmail.com>
2021-10-25 16:43:05 -0300

    Desafio 1: tentando corrigir erros de identaçao

--- desafio1.sql
+++ desafio1.sql
@@ -2,6 +2,7 @@
 SELECT COUNTRY_NAME, REGION_ID,
   CASE
     WHEN REGION_ID = 1 THEN 'incluído'
-ELSE 'não incluído'
+	ELSE 'não incluído'
 END AS REGION_ID FROM hr.countries ORDER BY COUNTRY_NAME ASC;
+
         
