a5a1af11ce747b9f3cc50ec88e68ca6cb14f6358
Icaro Melo <exemployt@gmail.com>
2021-10-25 20:21:05 -0300

    Requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT 
+    JOB_TITLE AS 'Cargo',
+    MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+    ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal',
+    ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal'
+FROM
+    hr.jobs
+ORDER BY Variação Salarial , JOB_TITLE;
