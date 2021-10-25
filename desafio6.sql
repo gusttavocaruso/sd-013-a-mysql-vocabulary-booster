3ab65fdfbc8c48ea435b400d497e98d15ac50cf1
Rafhael Gomes <rafhael@protonmail.com>
2021-10-25 17:49:15 -0300

    req 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     j.JOB_TITLE AS Cargo,
     jh.START_DATE AS 'Data de início do cargo',
-    d.DEPARTMENT_NAME
+    d.DEPARTMENT_NAME AS Departamento
 FROM
     hr.job_history jh
         INNER JOIN
