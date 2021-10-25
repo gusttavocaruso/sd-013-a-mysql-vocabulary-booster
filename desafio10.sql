4086489af8c5005dc24beec203eb6c1356e4c78a
Gustavo Mauricio <gmauricio207@gmail.com>
2021-10-25 16:19:42 -0300

    Req 2

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,12 @@
-/* 10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
-SELECT * FROM products;
-SELECT * FROM order_details;
+--  10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
 SELECT 
-    p.ProductName AS 'Produto', MIN(od.QUANTITY) AS 'Mínima'
+    p.ProductName AS Produto,
+    MIN(od.QUANTITY) AS Mínima,
+    MAX(od.Quantity) AS Máxima,
+    ROUND(AVG(od.Quantity), 2) AS Média
 FROM
     products AS p
         INNER JOIN
-    order_details AS od ON p.Productid = od.ProductID;*/
+    order_details AS od ON p.Productid = od.ProductID
+GROUP BY p.ProductName
+ORDER BY Média ASC , Produto ASC;
