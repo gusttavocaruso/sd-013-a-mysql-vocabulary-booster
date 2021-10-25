2112858ec69d916633e59cc45fd90fe83add4a6f
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-10-25 16:25:04 -0300

    requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+USE hr;
+SELECT 
+    JOB_TITLE AS CARGO,
+    MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM
+    jobs
+ORDER BY Diferença entre salários máximo e mínimo , JOB_TITLE;
