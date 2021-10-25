5019f3f81dc9b3fac9e8504f46a27c658fd045d6
João Paulo <jp.viegas@hotmail.com>
2021-10-25 20:28:54 -0300

    finalizando requisito 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
-SELECT country_name AS 'País', IF (regions.region_name = 'Europe', 'incluído', 'nao incluído') AS 'Status Inclusão'
+SELECT country_name AS 'País', IF (regions.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries, hr.regions
 WHERE (countries.region_id = regions.region_id);
