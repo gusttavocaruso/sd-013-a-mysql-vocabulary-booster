10b3c6852c6b635ca49f862549ddf780066dc097
Rafhael Gomes <rafhael@protonmail.com>
2021-10-25 14:34:08 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,19 @@
+SELECT 
+    JOB_TITLE AS 'Cargo',
+    CASE
+        WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+        WHEN
+            MAX_SALARY > 20000
+                AND MAX_SALARY <= 30000
+        THEN
+            'Alto'
+        WHEN
+            MAX_SALARY > 10000
+                AND MAX_SALARY <= 20000
+        THEN
+            'Médio'
+        ELSE 'Baixo'
+    END AS 'Nível'
+FROM
+    hr.jobs
+ORDER BY JOB_TITLE ASC;
