de186563201cb0033e2db4ade07b9786825c0cf4
Rafael Victor <rafavictorgs@gmail.com>
2021-10-25 18:24:46 -0300

    Resolve desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,13 @@
+SELECT JOB_TITLE AS Cargo,
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
+ORDER BY `Variação Salarial` ASC, Cargo;
+
+/*SELECT j.JOB_TITLE AS Cargo,
+(j.MAX_SALARY - j.MIN_SALARY) AS `Variação Salarial`,
+ROUND(j.MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+ROUND(j.MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs AS j
+ORDER BY `Variação Salarial`, Cargo;*/
