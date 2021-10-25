c8d9739adaf087d147e65919227ba9f95d84a9c8
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-10-25 15:58:04 -0300

    requisito 2/corrigido

--- desafio2.sql
+++ desafio2.sql
@@ -1,30 +1,20 @@
-SELECT
-  (
-    SELECT
-      JOB_TITLE
-    FROM
-      hr.jobs
-    WHERE
-      JOB_ID = e.JOB_ID
-    LIMIT
-      1
-  ) AS 'Cargo',
-  (
-    SELECT
-      CASE
-        WHEN e.SALARY BETWEEN 5000
-        AND 10000 THEN 'Baixo'
-        WHEN e.SALARY BETWEEN 10001
-        AND 20000 THEN 'Médio'
-        WHEN e.SALARY BETWEEN 20001
-        AND 30000 THEN 'Alto'
+SELECT 
+    (SELECT 
+            JOB_TITLE
+        FROM
+            hr.jobs
+        WHERE
+            JOB_ID = e.JOB_ID
+        LIMIT 1) AS 'Cargo',
+    (CASE
+        WHEN e.SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+        WHEN e.SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
+        WHEN e.SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
         WHEN e.SALARY > 30000 THEN 'Altíssimo'
         ELSE 'outro'
-      END
-  ) AS 'Nível'
+    END) AS 'Nível'
 FROM
-  hr.employees AS e
+    hr.employees AS e
 WHERE
-  'Nível' <> 'outro'
-ORDER BY
-  'Cargo'
+    e.SALARY >= 5000
+ORDER BY 'Cargo'
