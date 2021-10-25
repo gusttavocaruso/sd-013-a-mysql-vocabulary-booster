55df6d43f62ed44256b4e906cbac2bfddf95646e
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-10-25 18:23:10 -0300

    Agora vai req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT  
-  c.job_title AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média Salarial', CASE
+  c.job_title AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média salarial', CASE
   WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
   WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
   WHEN ROUND(AVG(e.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
