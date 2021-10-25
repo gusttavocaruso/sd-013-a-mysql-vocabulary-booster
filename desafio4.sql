28e13d027aac79cdb5b0b009c1d6a032cce3ecde
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-10-25 18:10:00 -0300

    Req 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT  
+  c.job_title AS 'Cargo', ROUND(AVG(e.salary)) AS 'Média Salarial', CASE
+  WHEN ROUND(AVG(e.salary)) BETWEEN 2000 AND 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(e.salary)) BETWEEN 5801 AND 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(e.salary)) BETWEEN 7501 AND 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(e.salary)) > 10500 THEN 'CEO'
+  END AS 'Senioridade'
+FROM
+  hr.jobs AS c
+    INNER JOIN
+  hr.employees AS e ON c.job_id = e.job_id
+GROUP BY Cargo
+ORDER BY ROUND(AVG(e.salary)) ASC, c.job_title ASC;
