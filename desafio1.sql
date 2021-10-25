993c25fc359555bbb05d9eafebdad47578a8c834
Anajulia-bs <juliehindius@gmail.com>
2021-10-25 17:24:55 -0300

    Desafio 2: nao sei o pq nao passa no codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -2,6 +2,6 @@ SELECT COUNTRY_NAME AS País,
 CASE
   WHEN REGION_ID = 1 THEN 'incluído'
   ELSE 'não incluído'
-END AS 'Status Inclusão'
+  END AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País;
