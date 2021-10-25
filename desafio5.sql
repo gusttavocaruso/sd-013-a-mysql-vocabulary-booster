371bca005e489b37cc47a168e1f2f65b01fbccc0
ArthurMGoulart <budiarthur@gmail.com>
2021-10-25 19:43:11 -0300

    Code climate

--- desafio5.sql
+++ desafio5.sql
@@ -2,4 +2,5 @@ SELECT JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
 MIN_SALARY DIV 12 AS 'Média mínima mensal',
 MAX_SALARY DIV 12 AS 'Média máxima mensal'
-FROM hr.jobs;
+FROM hr.jobs
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
