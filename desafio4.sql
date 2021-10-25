212247f67225d1464a0499b6c3257989ce86a684
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-10-25 18:18:47 -0300

    Correcao req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
 SELECT  
-  c.job_title AS 'Cargo', ROUND(AVG(e.salary)) AS 'Média Salarial', CASE
-  WHEN ROUND(AVG(e.salary)) BETWEEN 2000 AND 5800 THEN 'Júnior'
-  WHEN ROUND(AVG(e.salary)) BETWEEN 5801 AND 7500 THEN 'Pleno'
-  WHEN ROUND(AVG(e.salary)) BETWEEN 7501 AND 10500 THEN 'Sênior'
-  WHEN ROUND(AVG(e.salary)) > 10500 THEN 'CEO'
+  c.job_title AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média Salarial', CASE
+  WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(e.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
   END AS 'Senioridade'
 FROM
   hr.jobs AS c
