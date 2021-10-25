b47c2f2e4e0d9cf0043a4085bcc0566e928a170e
Bruno Pinho <brunopinho@outlook.com>
2021-10-25 17:13:34 -0300

    finalizado requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,13 @@
 SELECT
-   j.JOB_TITLE AS `Cargo`,
-ROUND(AVG(e.SALARY)) AS `Média salarial`
+	j.JOB_TITLE AS `Cargo`,
+	ROUND(AVG(e.SALARY)) AS `Média salarial`,
+    CASE 
+		WHEN ROUND(AVG(e.SALARY)) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(e.SALARY)) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(e.SALARY)) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN ROUND(AVG(e.SALARY)) > 10501 THEN 'CEO'
+        ELSE 'Estimando'
+        END AS `Senioridade`
 FROM
     hr.employees e
 INNER JOIN hr.jobs j ON e.JOB_ID = j.JOB_ID
