c91de8d86460617fa74150c504fccfb032e6544f
Thiago <thiagoc.carboneri@gmail.com>
2021-10-25 15:50:13 -0300

    requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT c.COUNTRY_NAME AS 'PAÍS',
+SELECT c.COUNTRY_NAME AS 'País',
   IF(r.REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries c
   INNER JOIN regions r ON c.REGION_ID = r.REGION_ID
