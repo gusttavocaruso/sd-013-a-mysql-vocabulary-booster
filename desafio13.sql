ea89b6ac0f3b9f02c4a2d1714e4969a69586885e
vdionysio <vini.dionysio@gmail.com>
2021-10-25 14:53:08 -0300

    req 13

---
+++ desafio13.sql
@@ -0,0 +1,11 @@
+SELECT
+	P.ProductName AS 'Produto',
+    P.Price AS 'Preço'
+FROM
+	w3schools.products AS P
+WHERE EXISTS (
+	SELECT * FROM w3schools.order_details AS OD
+    WHERE OD.Quantity > 80
+    AND OD.ProductID = P.ProductID
+)
+ORDER BY P.ProductName;
