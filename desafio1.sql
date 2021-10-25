e21e854e44e17b2b42901e2497e566dfcd36d696
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-25 15:22:08 -0300

    adiciona comentario

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,8 @@
 -- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
 
 /*
+A estrutura básica da nossa query:
+
 	SELECT (coluna 1 AS __, CASE coluna 2 (condição) AS __)
 	FROM (tabela 1) AS __
 	INNER JOIN (tabela 2) AS __
@@ -9,6 +11,8 @@
 */
 
 /*
+Substituindo com as informações extraidas do enunciado:
+
 	SELECT
 		C.COUNTRY_NAME AS País,
 		CASE R.REGION_NAME (condição) AS `Status Inclusão`
@@ -18,6 +22,7 @@
 	ORDER BY COUNTRY_NAME;
 */
 
+-- Por último, especificando a condição do valor da coluna `Status Inclusão`:
 SELECT
 	C.COUNTRY_NAME AS País,
 	CASE R.REGION_NAME
