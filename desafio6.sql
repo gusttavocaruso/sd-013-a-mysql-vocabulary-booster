c0e7f4aa29e9f0b327ba8e3e2e329145848e38db
Cássio Pereira <cassiorodp@gmail.com>
2021-10-25 16:54:17 -0300

    desafio 6 completo

--- desafio6.sql
+++ desafio6.sql
@@ -1,22 +1,3 @@
-/*SELECT 
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-    (SELECT 
-            JOB_TITLE
-        FROM
-            hr.job_history AS j
-        WHERE
-            j.JOB_ID = e.JOB_ID) AS Cargo,
-    e.HIRE_DATE AS `Data de início do cargo`,
-    (SELECT 
-            d.DEPARTMENT_NAME
-        FROM
-            hr.departments AS d
-        WHERE
-            d.DEPARTMENT_ID = e.DEPARTMENT_ID) AS Departamento
-FROM
-    hr.employees AS e
-ORDER BY `Nome completo` DESC , Departamento ASC;
-*/
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
     j.JOB_TITLE AS Cargo,
@@ -30,4 +11,4 @@ FROM
     hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
         INNER JOIN
     hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, Departamento ASC;
+ORDER BY `Nome completo` DESC, Cargo ASC;
