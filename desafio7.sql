b46e35ff0ebe32c8932f293d2a5e14a0f9ea2417
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-10-25 17:33:35 -0300

    Requisito 8

--- desafio7.sql
+++ desafio7.sql
@@ -3,7 +3,5 @@ h.START_DATE AS 'Data de início', e.SALARY AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN  hr.job_history AS h
 ON h.EMPLOYEE_ID = e.EMPLOYEE_ID 
-INNER JOIN  hr.jobs AS j 
-ON j.JOB_ID = e.JOB_ID
 WHERE MONTH(h.START_DATE) IN(1, 2, 3) 
 ORDER BY CONCAT(e.FIRST_NAME,' ',e.LAST_NAME), h.START_DATE;
