8ec285edb3ad108793122d9d924e4eb64de61786
junglejf <jfcchagas@id.uff.br>
2021-10-25 14:18:58 -0300

    Desafio 1: Mostrar se o país pertence ou não a Europa

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT country_name as 'País',
+IF(region_name = 'Europe', 'incluído','não incluído') as 'Status Inclusão'
+FROM countries AS C, regions as R 
+WHERE C.region_id = R.region_id
+ORDER BY country_name;
+
