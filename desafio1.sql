2f35aa7a52496e0aebeb00e6aa99befea58b8ef0
suco <francalucas937@gmail.com>
2021-10-25 18:24:05 -0300

    requ 1 feito

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,9 @@
 SELECT 
-    C.COUNTRY_NAME,
+    C.COUNTRY_NAME AS 'País',
     (CASE
-        WHEN C.REGION_ID = 1 THEN 'pertence'
-        ELSE 'nao pertence'
-    END)
+        WHEN C.REGION_ID = 1 THEN 'incluído'
+        ELSE 'não incluído'
+    END) AS 'Status Inclusão'
 FROM
     hr.countries AS C
     ORDER BY COUNTRY_NAME;
