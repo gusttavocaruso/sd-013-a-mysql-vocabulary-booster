43a712450b998ba99c6ffef18a5fc19ede04dbf8
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-10-25 15:00:42 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(FIRST_NAME, ' ',LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.employees e, hr.job_history jh
+WHERE (MONTH(jh.START_DATE) = 1 OR MONTH(jh.START_DATE) = 2 OR MONTH(jh.START_DATE) = 3)
+AND e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Data de início`;
