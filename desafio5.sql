329513c64d4db2eacf35a211fe9443bdca946c21
ArthurMGoulart <budiarthur@gmail.com>
2021-10-25 19:41:21 -0300

    Realizando requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,5 @@
 SELECT JOB_TITLE AS Cargo,
-MAX_SALARY - MIN_SALARY AS 'Variação Salarial'
+MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+MIN_SALARY DIV 12 AS 'Média mínima mensal',
+MAX_SALARY DIV 12 AS 'Média máxima mensal'
 FROM hr.jobs;
-SELECT * FROM hr.jobs;
