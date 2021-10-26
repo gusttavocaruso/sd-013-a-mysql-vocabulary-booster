a4551959590426f4861bb9c465caa66479ed3c4b
Cae <eduardokae01@hotmail.com>
2021-10-26 14:35:00 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo',
+COUNT(O.OrderID)
+FROM w3schools.employees E
+INNER JOIN w3schools.orders O
+ON E.EmployeeID = O.EmployeeID
+GROUP BY E.EmployeeID
+HAVING COUNT(O.OrderID) > 0
+ORDER BY COUNT(O.OrderID);
