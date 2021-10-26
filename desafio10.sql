e56ac068cb0991b4711bdb7f32b89a3a7f2dc829
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-25 21:28:11 -0300

    code climate pissing me off

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,5 @@
-SELECT
-	  p.ProductName as Produto,
-    MIN(o.Quantity) as Mínima,
-    MAX(o.Quantity) as Máxima,
-    ROUND(AVG(o.Quantity),2) as Média
-FROM
-    w3schools.order_details as o
+SELECT p.ProductName as Produto, MIN(o.Quantity) as Mínima, MAX(o.Quantity) as Máxima, ROUND(AVG(o.Quantity),2) as Média
+FROM w3schools.order_details as o
 INNER JOIN w3schools.products as p ON o.ProductID = p.ProductID
 GROUP BY p.ProductName
 HAVING Média > 20.00
