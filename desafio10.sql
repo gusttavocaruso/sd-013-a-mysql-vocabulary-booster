5e62e06989975cd4946d3de5bfb907527cb3c174
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-25 21:15:55 -0300

    challenge 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+	p.ProductName as Produto,
+    MIN(o.Quantity) as Mínima,
+    MAX(o.Quantity) as Máxima,
+    ROUND(AVG(o.Quantity),2) as Média
+FROM
+    w3schools.order_details as o
+INNER JOIN w3schools.products as p ON o.ProductID = p.ProductID
+GROUP BY p.ProductName
+HAVING Média > 20.00
+ORDER BY Média;
