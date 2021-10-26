90c6ba6e5eb22448f7731d0c0e1e5c7f04306bae
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-10-26 12:06:47 -0300

    Correcao req 8

--- desafio8.sql
+++ desafio8.sql
@@ -10,4 +10,4 @@ INNER JOIN w3schools.shippers AS s
 ON o.shipperId = s.shipperId
 WHERE s.ShipperName = 'Speedy Express' OR  s.ShipperName = 'United Package'
 
-ORDER BY c.ContactName ASC, o.OrderDate ASC;
+ORDER BY c.ContactName ASC, s.ShipperName ASC, o.OrderDate ASC;
