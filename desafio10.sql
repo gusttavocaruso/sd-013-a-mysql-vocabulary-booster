0a6795841182b4f9453b6070346aeb0dc7c86cae
Cae <eduardokae01@hotmail.com>
2021-10-26 14:47:09 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT ProductName,
+MIN(DETAILS.Quantity) AS Mínima,
+MAX(DETAILS.Quantity) AS Máxima,
+ROUND(AVG(Quantity), 2)
+FROM w3schools.products AS PRODUCT
+INNER JOIN w3schools.order_details AS DETAILS
+ON PRODUCT.ProductID = DETAILS.ProductID
+GROUP BY ProductName
+HAVING AVG(Quantity) > 20
+ORDER BY AVG(Quantity),
+ProductName;
