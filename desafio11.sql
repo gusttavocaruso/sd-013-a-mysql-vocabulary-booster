d86293ada9fca0bbcf43becb4cd6fe9f582280c1
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-26 00:34:29 -0300

    challenge 11 again

--- desafio11.sql
+++ desafio11.sql
@@ -9,4 +9,11 @@ SELECT
             t1.Country = t2.Country) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS t1
+WHERE
+    (SELECT
+            (COUNT(t2.Country) - 1)
+        FROM
+            w3schools.customers AS t2
+        WHERE
+            t1.Country = t2.Country) > 0
 ORDER BY t1.ContactName;
