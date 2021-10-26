7d4c823b93257f3aa32e19c9ba7dc071fe63a294
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-25 22:21:00 -0300

    choosing the correct column for contactName in challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,13 @@
 SELECT
-    c.CustomerName AS 'Nome de contato',
+    c.ContactName AS 'Nome de contato',
     s.ShipperName AS 'Empresa que fez o envio',
     o.OrderDate AS 'Data do pedido'
 FROM
     w3schools.orders AS o
         INNER JOIN
-    w3schools.customers AS c ON o.CustomerID = c.CustomerID
+    w3schools.customers AS c ON c.CustomerID = o.CustomerID
         INNER JOIN
-    w3schools.shippers AS s ON o.ShipperID = s.ShipperID
-WHERE s.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY c.CustomerName , s.ShipperName , o.OrderDate;
+    w3schools.shippers AS s ON s.ShipperID = o.ShipperID
+WHERE
+    o.ShipperID IN (1 , 2)
+ORDER BY c.ContactName , s.ShipperName , o.OrderDate;
