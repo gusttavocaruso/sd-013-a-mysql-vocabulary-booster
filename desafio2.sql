ea4876940f4d40c3ea80034363bfb74602593c4c
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-10-26 19:52:14 -0300

    arrumando req 1, 2 e 3

--- desafio2.sql
+++ desafio2.sql
@@ -3,7 +3,7 @@ CASE
 WHEN max_salary > 5000 AND max_salary < 10000 THEN 'Baixo'
 WHEN max_salary > 10001 AND max_salary < 20000 THEN 'Médio'
 WHEN max_salary > 20001 AND max_salary < 30000 THEN 'Alto'
-ELSE 'Altíssimo'
+WHEN max_salary > 30000 THEN 'Altíssimo'
 END AS Nível
 FROM hr.jobs
 ORDER BY job_title;
--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,4 @@
-SELECT
-job_title AS Cargo,
-max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
+SELECT job_title AS Cargo,
+max_salary - min_salary AS `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
-ORDER BY max_salary ASC, Cargo ASC;
+ORDER BY `Diferença entre salários máximo e mínimo` ASC, Cargo ASC;
