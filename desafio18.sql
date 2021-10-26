0ebc1c47ad20ed093d1eb39d0c717c857448d8aa
Icaro Ferreira <icaro.iky@gmail.com>
2021-10-26 16:40:33 -0300

    Req 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,3 +1,4 @@
+-- https://www.visualdicas.com.br/programacao/php/28-data-mysql-para-formato-br
 SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
 date_format(job.START_DATE, '%d/%m/%Y') AS 'Data de início',
 date_format(job.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
@@ -6,4 +7,3 @@ FROM hr.job_history AS `job`
 INNER JOIN hr.employees AS emp
 ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
 ORDER BY `Nome completo`;
--- https://www.visualdicas.com.br/programacao/php/28-data-mysql-para-formato-br
