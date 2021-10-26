bbab7491bcd91d03ddbb2fb67181d57f4503585d
Isabella Antunes <bella.antunes2@gmail.com>
2021-10-26 12:25:26 -0300

    Req 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM employees;
+-- SELECT * FROM job_history;
+SELECT UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+jh.START_DATE AS 'Data de início',
+e.SALARY AS Salário
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
