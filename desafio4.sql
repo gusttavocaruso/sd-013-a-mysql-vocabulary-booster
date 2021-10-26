e216d5c8d4dd7be81fd8779349d8f25315a5e00e
Cae <eduardokae01@hotmail.com>
2021-10-25 21:32:22 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT J.JOB_TITLE AS Cargo,
+AVG(ROUND(E.SALARY, 2)) AS 'Média salarial',
+CASE
+  WHEN AVG(E.SALARY) < 5800 THEN 'Júnior'
+  WHEN AVG(E.SALARY) < 7500 THEN 'Pleno'
+  WHEN AVG(E.SALARY) < 10500 THEN 'Sênior'
+ELSE 'CEO'
+END AS Senioridade
+FROM hr.employees AS E
+INNER JOIN hr.jobs AS J
+ON E.JOB_ID = J.JOB_ID
+GROUP BY J.JOB_ID;
+
