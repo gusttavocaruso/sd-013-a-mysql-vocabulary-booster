a9db338fdfa6cac981f10f424a7e768931682a15
turtore <brunosap@gmail.com>
2021-10-26 17:58:12 -0300

    resolvida questao 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,10 @@
-SELECT c2.CustomerName AS 'Nome',
-c2.country AS 'País',
-(SELECT 
-COUNT(*) FROM w3schools.customers as c1
-WHERE c1.country = c2.country
-AND c1.ContactName != c2.ContactName
-GROUP BY c1.country
-) AS 'Número de compatriotas'
+SELECT c.CustomerName AS 'Nome',
+c.country AS 'País',
+(
+SELECT COUNT(Country)-1 
 FROM w3schools.customers AS c2
-HAVING 'Número de compatriotas'
-ORDER BY c2.CustomerName;
+WHERE c.country = c2.Country
+) 
+AS 'Número de compatriotas'
+FROM w3schools.customers AS c
+ORDER BY CustomerName;
