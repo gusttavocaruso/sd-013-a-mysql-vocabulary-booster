f0ed1355e4499e869cb35c96c51c9f2df5aab2c1
rogeriop1990cv <rogeriop1990cv@gmail.com>
2021-10-26 04:35:02 -0300

    tentando requisot 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT
+    concat(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+    jobs.JOB_TITLE AS `Cargo`,
+    employees.HIRE_DATE AS `Data de início do cargo`,
+    departments.DEPARTMENT_NAME AS `Departamento`
+FROM
+    hr.employees AS employees
+    INNER JOIN hr.jobs AS jobs ON jobs.JOB_ID = employees.JOB_ID
+    INNER JOIN hr.departments AS departments ON departments.DEPARTMENT_ID = employees.DEPARTMENT_ID
+ORDER BY
+    `Nome completo`,
+    `Cargo`;
