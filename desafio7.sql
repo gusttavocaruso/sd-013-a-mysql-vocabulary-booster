1872e8659fcfd7180de09f4b7a5484a8be383c2a
Cae <eduardokae01@hotmail.com>
2021-10-26 00:18:57 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+SELECT * FROM hr.job_history
+order by EMPLOYEE_ID;
+SELECT * FROM hr.employees;
+
+SELECT UCASE(
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)
+) AS 'Nome completo',
+H.START_DATE AS 'Data de início',
+SALARY AS 'Salário'
+FROM hr.employees AS E
+RIGHT JOIN hr.job_history AS H
+ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE MONTH(H.START_DATE) BETWEEN 01 AND 03
+ORDER BY CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME),
+H.START_DATE;
