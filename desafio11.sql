fc3f158d899e5acfd9a2d30f3160fa58f5937e28
turtore <brunosap@gmail.com>
2021-10-26 19:19:32 -0300

    atende requisito 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,10 @@
-SELECT c.CustomerName AS 'Nome',
+SELECT c.ContactName AS 'Nome',
 c.country AS 'País',
 (
-SELECT COUNT(Country)-1 
+SELECT COUNT(Country)
 FROM w3schools.customers AS c2
-WHERE c.country = c2.Country
+WHERE c.country = c2.Country AND  c.CustomerID != c2.CustomerID
 ) 
 AS 'Número de compatriotas'
 FROM w3schools.customers AS c
-ORDER BY CustomerName;
+ORDER BY c.ContactName;
