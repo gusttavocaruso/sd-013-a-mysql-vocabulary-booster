3856128098bd346234e4a632bb357a3bcbdd8dbc
Gustavo Mauricio <gmauricio207@gmail.com>
2021-10-26 08:59:51 -0300

    Req 17

---
+++ desafio12.sql
@@ -0,0 +1,3 @@
+-- 12 - Faça um relatório que lista todas as pessoas funcionárias que possuem o mesmo cargo
+
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo funcionário 1', salary AS 'Salário funcionário 1', phone_number AS 'Telefone funcionário 1' FROM hr.employees; 
