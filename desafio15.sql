5a2be5a86e810c7732ef2ba422627ad71b791cdf
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-10-26 19:25:01 -0300

    codeclimate update

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobName VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(E.Salary), 2) AS 'Média salarial'
+  SELECT ROUND(AVG(E.Salary), 2) AS 'Média salarial'
     FROM hr.employees AS E
     INNER JOIN hr.jobs AS J ON E.job_id = J.job_id
     WHERE J.job_title = jobName;
