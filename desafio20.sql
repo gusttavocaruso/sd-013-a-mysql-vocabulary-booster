b4dd23a84e05ebfe0c80905cf4641c91040d990d
Luiza Antiques <luiza.antiques@gmail.com>
2021-10-25 22:08:04 -0300

    Corrige desafio 20

--- desafio20.sql
+++ desafio20.sql
@@ -7,7 +7,7 @@ d.DEPARTMENT_NAME AS Departamento, j.JOB_TITLE AS Cargo
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-INNER JOIN hr.jobs AS js
+INNER JOIN hr.jobs AS j
 ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d
 ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
