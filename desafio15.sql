c64300ab51e478d014a918b9c7b3f3f8a55f4498
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-26 16:02:02 -0300

    challenge 15 code climate adjustments

--- desafio15.sql
+++ desafio15.sql
@@ -2,14 +2,10 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN strCargo VARCHAR(50))
 BEGIN
-	SELECT
-		ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-	FROM
-		hr.employees AS e
-			INNER JOIN
-		hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-	WHERE
-		j.JOB_TITLE = strCargo;
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = strCargo;
 END $$
 
 DELIMITER ;
