3a070c7feb251c45ab1edf0f21ffdd0c1d8ac326
Rafael Victor <rafavictorgs@gmail.com>
2021-10-26 19:28:52 -0300

    Refatorando desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(UCASE(FIRST_NAME), ' ', UCASE(LAST_NAME)) AS `Nome completo`,
 jobh.START_DATE AS `Data de início`,
-emp.SALARY AS Salário
+emp.SALARY AS `Salário`
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS jobh
 ON emp.EMPLOYEE_ID = jobh.EMPLOYEE_ID
