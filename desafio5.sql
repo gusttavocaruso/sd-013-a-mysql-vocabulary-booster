b7664044c458d97bb0f2fc34f4dcb0d295764590
rogeriop1990cv <rogeriop1990cv@gmail.com>
2021-10-26 03:48:59 -0300

    Requisito 5 refatoração

--- desafio5.sql
+++ desafio5.sql
@@ -1,13 +1,12 @@
 SELECT
-    jobs.JOB_TITLE AS 'Cargo',
-    jobs.MAX_SALARY - jobs.MIN_SALARY AS 'Variação Salarial',
-    round(AVG(jobs.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
-    round(AVG(jobs.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+    jobs.JOB_TITLE AS `Cargo`,
+    jobs.MAX_SALARY - jobs.MIN_SALARY AS `Variação Salarial`,
+    round(AVG(jobs.MIN_SALARY / 12), 2) AS `Média mínima mensal`,
+    round(AVG(jobs.MAX_SALARY / 12), 2) AS `Média máxima mensal`
 FROM
-    hr.employees AS employees
-    INNER JOIN hr.jobs AS jobs ON jobs.JOB_ID = employees.JOB_ID
+    hr.jobs AS jobs
 GROUP BY
-    employees.JOB_ID
+    jobs.JOB_ID
 ORDER BY
-    jobs.MAX_SALARY - jobs.MIN_SALARY,
-    jobs.JOB_TITLE;
+    `Variação Salarial`,
+    `Cargo`;
