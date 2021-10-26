235673282fe1ff8e4448aa4ea8956c9044d1aa92
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-10-25 23:39:58 -0300

    adiciona linha no final do bloco

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,18 @@
 -- 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
+
+/*
+1. Estrutura básica da query:
+
+  SELECT 
+      (coluna_1) AS 'Produto',
+      (coluna_2) AS 'Mínima',
+      (coluna_3) AS 'Máxima',
+      (coluna_4) AS 'Média'
+  FROM
+      (tabela_1) AS __
+          INNER JOIN
+      (tabela_2) AS __ ON (condição)
+  GROUP BY __
+  HAVING (condição)
+  ORDER BY `Média`, `Produto`;
+*/
