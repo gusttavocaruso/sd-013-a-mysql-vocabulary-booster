53f0886fa3de39bc9065bac7f67a507e4ba68fc8
Isabella Antunes <bella.antunes2@gmail.com>
2021-10-26 14:16:12 -0300

    Req 9

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM employees;
+-- SELECT * FROM orders;
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+COUNT(o.OrderID) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
