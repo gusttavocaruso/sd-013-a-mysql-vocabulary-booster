53b09384382631077182c9d6a700516ffc4e2716
Julia Baptista <baptista.julia@yahoo.com.br>
2021-10-25 22:07:18 -0300

    ajustes de codeclimat

--- desafio15.sql
+++ desafio15.sql
@@ -4,9 +4,9 @@ DELIMITER $$;
 
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) `Média salarial`
-    from hr.employees
-    WHERE JOB_ID = (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = job);
+SELECT ROUND(AVG(SALARY), 2) `Média salarial`
+from hr.employees
+WHERE JOB_ID = (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = job);
 END $$;
 	
 DELIMITER ;
