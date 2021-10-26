2e69baff4cd9988ecd0c5339746b73a16bcbd347
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-10-26 19:35:41 -0300

    arrumando req 16

--- desafio16.sql
+++ desafio16.sql
@@ -4,9 +4,9 @@ RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total_empregos INT;
 SELECT COUNT(*) FROM hr.job_history AS h
-INNER JOIN hr.employees AS e ON h.EmployeeID = e.EmployeeID
+INNER JOIN hr.employees AS e ON h.employee_id = e.employee_id
 WHERE e.email = email
-GROUP BY h.EmployeeID
+GROUP BY h.employee_id
 INTO total_empregos;
 RETURN total_empregos;
 END
