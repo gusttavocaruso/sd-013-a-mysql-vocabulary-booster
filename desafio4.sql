ea8bd849447f5e7ed5ca2ae7151a1e8236d56e44
Joao Santos <jvitor.spaula@gmail.com>
2021-10-26 14:05:30 -0300

    req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,18 @@
 SELECT 
-    job_title AS Cargo,
-    (max_salary - min_salary) AS 'Média salarial',
+    ho.job_title AS Cargo,
+    ROUND(AVG(he.salary), 2) AS 'Média salarial',
     CASE
-        WHEN ROUND(max_salary - min_salary, 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-        WHEN ROUND(max_salary - min_salary, 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN ROUND(max_salary - min_salary, 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-        WHEN ROUND(max_salary - min_salary, 2) > 10500 THEN 'CEO'
+        WHEN AVG(he.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(he.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(he.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN AVG(he.salary) > 10500 THEN 'CEO'
     END AS Senioridade
 FROM
-    hr.jobs
-    ORDER BY 'Média salarial' ASC, job_title ASC;
+    hr.employees AS he
+        INNER JOIN
+    hr.jobs AS ho ON he.job_id = ho.job_id
+GROUP BY ho.job_id
+ORDER BY 'Média salarial' ASC , ho.job_title ASC;
+
+-- Este requisito eu resolvi consultando o repositorio do mateus henrique T-13-A : 
+-- https://github.com/tryber/sd-013-a-mysql-vocabulary-booster/blob/matheushtc-mysql-vocabulary-booster/desafio4.sql
