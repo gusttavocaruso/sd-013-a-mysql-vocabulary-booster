23fb8eb8d64e99481e876cd9409c90fd2107f0cf
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-25 22:00:53 -0300

    tryng challenge 7 with jobs_history which doesnt make sense

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,11 @@
 SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    e.HIRE_DATE AS 'Data de início',
+    h.START_DATE AS 'Data de início',
     e.SALARY AS 'Salário'
 FROM
-    hr.employees AS e
+    hr.job_history AS h
+        INNER JOIN
+    hr.employees AS e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 WHERE
-    MONTH(e.HIRE_DATE) IN (1 , 2, 3)
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , e.HIRE_DATE;
+    MONTH(h.START_DATE) IN (1 , 2, 3)
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , h.START_DATE;
