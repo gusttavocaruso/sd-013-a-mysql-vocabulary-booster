57357b21f5eaec0154d3f3839b4f21ee64f31169
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-10-26 14:42:01 -0300

    Req 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT 
+p.productName AS 'Produto',
+MIN(od.quantity) AS 'Mínima',
+MAX(od.quantity) AS 'Máxima',
+ROUND(AVG(od.quantity),2) AS 'Média'
+
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+GROUP BY od.ProductID
+HAVING AVG(od.quantity) > 20
+
+ORDER BY AVG(od.quantity) ASC, p.productName ASC;
