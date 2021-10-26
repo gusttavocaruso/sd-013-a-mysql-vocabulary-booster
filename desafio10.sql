cd3d7ab4b2bd67268ae147d1c856725468c3bcc9
Cae <eduardokae01@hotmail.com>
2021-10-26 18:59:00 -0300

    refatora desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
-SELECT ProductName,
+SELECT ProductName AS Produto,
 MIN(DETAILS.Quantity) AS Mínima,
 MAX(DETAILS.Quantity) AS Máxima,
-ROUND(AVG(Quantity), 2)
+ROUND(AVG(Quantity), 2) AS Média
 FROM w3schools.products AS PRODUCT
 INNER JOIN w3schools.order_details AS DETAILS
 ON PRODUCT.ProductID = DETAILS.ProductID
