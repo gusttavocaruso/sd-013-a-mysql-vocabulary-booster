7ff8a0b5f7bf4ee6c80280ff3aa5e494fefc63a3
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-10-25 21:05:15 -0300

    requisito 10

--- desafio11.sql
+++ desafio11.sql
@@ -2,7 +2,7 @@ SELECT
     p.ProductName AS 'Produto',
     MIN(od.Quantity) AS 'Mínima',
     MAX(od.Quantity) AS 'Máxima',
-    round(AVG(od.Quantity), 2) AS 'Média'
+    AVG(od.Quantity) AS 'Média'
 FROM
     w3schools.order_details AS od
         INNER JOIN
