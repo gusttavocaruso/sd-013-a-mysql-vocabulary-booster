6e6ca842d6faed8cf184d56f15b01c6450a7ca1f
Rafhael Gomes <rafhael@protonmail.com>
2021-10-26 19:03:16 -0300

    fix: requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,21 @@
 SELECT 
     t1.ContactName AS Nome,
     t1.Country AS País,
-    (SELECT 
-            COUNT(*)
+    ((SELECT 
+            COUNT(*) AS Quantity
         FROM
             w3schools.customers t2
         WHERE
             t1.Country = t2.Country
-        GROUP BY t2.Country) - 1 AS 'Número de compatriotas'
-FROM w3schools.customers t1
-ORDER BY t1.ContactName ASC;
+        GROUP BY t2.Country) - 1) AS 'Número de compatriotas'
+FROM
+    w3schools.customers t1
+WHERE
+    (SELECT 
+            COUNT(*)
+        FROM
+            w3schools.customers t3
+        WHERE
+            t1.Country = t3.Country
+        GROUP BY t3.Country) > 1
+ORDER BY t1.ContactName ASC
