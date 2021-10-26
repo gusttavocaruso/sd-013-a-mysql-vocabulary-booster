5368aa2de2f30bef925e2d5e6c89ded4eb13c9b8
LuizFJP <luizfjportela@gmail.com>
2021-10-25 22:22:43 -0300

    requirement 09

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+SELECT 
+	CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+	COUNT(o.OrderID) AS `Total de pedidos`
+FROM 
+	employees e
+INNER JOIN
+	orders o
+ON
+	e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
+	
