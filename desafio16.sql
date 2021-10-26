1a3a8be22aaccfc38d2a8565f18100bb9f124172
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-10-26 14:33:52 -0300

    Corrige 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,13 +1,15 @@
-USE hr; 
-DELIMITER $$ 
+USE hr
+DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(100))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN 
-    SELECT COUNT(*) AS total_empregos
+    DECLARE qts_jobs INT;
+    SELECT COUNT(*) 
     FROM hr.employees AS e
     INNER JOIN  hr.job_history AS jh 
     ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-    WHERE e.EMAIL = email;    
+    WHERE e.EMAIL = email INTO @qtd_jobs;
+    RETURN @qtd_jobs;    
 END $$ 
 
-DELIMITER ;
