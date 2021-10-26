c371f76b0fbc066f50e78be99cc77306b8af734f
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-25 22:39:15 -0300

    one more time challenge 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,11 +2,11 @@ SELECT
     p.ProductName AS Produto,
     MIN(o.Quantity) AS Mínima,
     MAX(o.Quantity) AS Máxima,
-    CONVERT( ROUND(AVG(o.Quantity), 2) , CHAR (10)) AS Média
+    ROUND(AVG(o.Quantity), 2) AS Média
 FROM
     w3schools.order_details AS o
         INNER JOIN
     w3schools.products AS p ON o.ProductID = p.ProductID
 GROUP BY p.ProductName
 HAVING Média > 20
-ORDER BY Média;
+ORDER BY Média, Produto;
