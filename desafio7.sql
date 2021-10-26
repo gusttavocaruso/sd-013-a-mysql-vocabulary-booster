d5ead34a6faae25d92ee8bd39718ef989ecf8e7d
Cae <eduardokae01@hotmail.com>
2021-10-26 00:33:08 -0300

    desafio 7 refatorado

--- desafio7.sql
+++ desafio7.sql
@@ -1,12 +1,8 @@
-SELECT * FROM hr.job_history
-order by EMPLOYEE_ID;
-SELECT * FROM hr.employees;
-
 SELECT UCASE(
   CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)
 ) AS 'Nome completo',
 H.START_DATE AS 'Data de início',
-SALARY AS 'Salário'
+SALARY AS Salário
 FROM hr.employees AS E
 RIGHT JOIN hr.job_history AS H
 ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
