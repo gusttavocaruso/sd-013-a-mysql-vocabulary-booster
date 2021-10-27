d0ad712888db823131fad3bc3a9f44fabe56c3c1
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-10-27 11:13:11 -0300

    Req 15

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo varchar(45))
+BEGIN
+  SELECT 
+    ROUND(AVG(e.salary),2) AS 'Média Salarial' 
+  FROM hr.employees AS e 
+  INNER JOIN hr.jobs AS j
+  ON j.job_title = cargo AND e.job_id = j.job_id;
+END$$ 
+
+DELIMITER ;
+
+call buscar_media_por_cargo('Programmer');
