f5a50e7ffd1591a2ab0c27f9a2935989adff4fe9
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-27 10:12:02 -0300

    code climate adjustments for challenge 19

--- desafio19.sql
+++ desafio19.sql
@@ -4,10 +4,10 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE pessoas_contratadas INT;
-	SELECT COUNT(*)
-	FROM hr.employees
-	WHERE MONTH(DATE(HIRE_DATE)) = mes AND YEAR(DATE(HIRE_DATE)) = ano
-	INTO pessoas_contratadas;
+SELECT COUNT(*)
+FROM hr.employees
+WHERE MONTH(DATE(HIRE_DATE)) = mes AND YEAR(DATE(HIRE_DATE)) = ano
+INTO pessoas_contratadas;
 RETURN pessoas_contratadas;
 END $$
 
