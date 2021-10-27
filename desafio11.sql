bf57c9d75653d53d39f2582522ede23249d480a9
Linux <ivan@pop-os.localdomain>
2021-10-27 11:36:09 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,5 +2,9 @@ SELECT c.contactName AS 'Nome',
 c.Country AS 'País',
 (SELECT COUNT(*) - 1 FROM w3schools.customers AS c2
 WHERE c2.Country = c.Country) AS 'Número de compatriotas' 
-FROM w3schools.customers AS c
-ORDER BY Nome;
+FROM w3schools.customers AS c 
+WHERE
+(SELECT COUNT(*) - 1 FROM w3schools.customers AS c2
+WHERE c2.Country = c.Country
+GROUP BY c2.Country) > 0
+ORDER BY `Nome`;
