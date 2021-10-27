e276a733d577fac8ae0f59679b1290c4e3c0e122
Linux <ivan@pop-os.localdomain>
2021-10-26 22:45:12 -0300

    Requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,5 @@ jh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) IN (01,02,03)
 ORDER BY `Nome completo` ASC, `Data de início` DESC;
-
