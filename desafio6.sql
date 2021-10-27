872d067a562c9c70251328bc9549cdf41cd63e49
Linux <ivan@pop-os.localdomain>
2021-10-26 22:22:24 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -6,4 +6,4 @@ FROM hr.job_history AS jh
 INNER JOIN hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
-ORDER BY 'Nome completo' DESC, Cargo ASC;
+ORDER BY `Nome completo` DESC, Cargo ASC;
