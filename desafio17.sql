f805a7c31b43151bf337314850ac66b98706b320
Cae <eduardokae01@hotmail.com>
2021-10-27 09:13:54 -0300

    refatora desafio 17 parte 2

--- desafio17.sql
+++ desafio17.sql
@@ -5,8 +5,7 @@ CREATE TRIGGER update_orderDate
   BEFORE INSERT ON orders
   FOR EACH ROW
 BEGIN
-  INSERT INTO orders(OrderDate)
-  VALUES(NOW());
-END $$
+  SET NEW.OrderDate =NOW();
+END $$;
 
 DELIMITER ;
