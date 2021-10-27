79cd6eaca4a33adb5f34d8fa59af98beff806d1d
turtore <brunosap@gmail.com>
2021-10-27 11:22:46 -0300

    fix climate

--- desafio15.sql
+++ desafio15.sql
@@ -4,11 +4,11 @@ DELIMITER $$;
 CREATE PROCEDURE buscar_media_por_cargo(IN nome_do_cargo VARCHAR(25))
 
 BEGIN
-	SELECT ROUND(AVG(salary), 2) AS 'Média salarial' FROM employees AS e
-    INNER JOIN jobs as j
-    ON e.job_id = j.job_id
-    WHERE job_title LIKE nome_do_cargo
-    GROUP BY j.job_title;    
+SELECT ROUND(AVG(salary), 2) AS 'Média salarial' FROM employees AS e
+INNER JOIN jobs as j
+ON e.job_id = j.job_id
+WHERE job_title LIKE nome_do_cargo
+GROUP BY j.job_title;    
 END $$;
 
 DELIMITER ;
