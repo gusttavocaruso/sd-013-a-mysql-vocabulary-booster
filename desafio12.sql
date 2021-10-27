d5e31bdeff34f648e31cd33212c171211786b1b1
junglejf <jfcchagas@id.uff.br>
2021-10-26 23:47:14 -0300

    Codeclimate fix

--- desafio12.sql
+++ desafio12.sql
@@ -1,12 +1,12 @@
 SELECT 
-	CONCAT(E1.first_name, ' ', E1.last_name) as `Nome completo funcionário 1`,
-    E1.salary as `Salário funcionário 1`,
-    E1.phone_number as `Telefone funcionário 1`,
-    CONCAT(E2.first_name, ' ', E2.last_name) as `Nome completo funcionário 2`,
-	E2.salary as `Salário funcionário 2`,
-    E2.phone_number as `Telefone funcionário 2`
+  CONCAT(E1.first_name, ' ', E1.last_name) as `Nome completo funcionário 1`,
+  E1.salary as `Salário funcionário 1`,
+  E1.phone_number as `Telefone funcionário 1`,
+  CONCAT(E2.first_name, ' ', E2.last_name) as `Nome completo funcionário 2`,
+  E2.salary as `Salário funcionário 2`,
+  E2.phone_number as `Telefone funcionário 2`
 FROM
-	hr.employees as E1, hr.employees as E2
+  hr.employees as E1, hr.employees as E2
 WHERE 
-	E1.job_id = E2.job_id AND CONCAT(E1.first_name, ' ', E1.last_name) <> CONCAT(E2.first_name, ' ', E2.last_name)
+  E1.job_id = E2.job_id AND CONCAT(E1.first_name, ' ', E1.last_name) <> CONCAT(E2.first_name, ' ', E2.last_name)
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
--- desafio13.sql
+++ desafio13.sql
@@ -1,8 +1,8 @@
 SELECT 
-	P.ProductName as Produto, 
-    P.price as Preço
+  P.ProductName as Produto, 
+  P.price as Preço
 FROM
-	w3schools.products as P, w3schools.orders as O, w3schools.order_details as OD
+  w3schools.products as P, w3schools.orders as O, w3schools.order_details as OD
 WHERE
-	P.ProductID = OD.ProductID AND O.OrderID = OD.OrderID AND OD.quantity > 80
+  P.ProductID = OD.ProductID AND O.OrderID = OD.OrderID AND OD.quantity > 80
 ORDER BY Produto;
