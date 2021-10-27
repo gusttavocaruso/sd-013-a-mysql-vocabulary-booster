4b40d2283c6e383a355f3640ad3157d58f6d3884
Linux <ivan@pop-os.localdomain>
2021-10-27 10:58:16 -0300

    Requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT p.ProductName AS 'Produto',
 MIN(o.quantity) AS 'Mínima',
 MAX(o.quantity) AS 'Máxima',
-AVG(o.quantity) AS 'Média'
+ROUND(AVG(o.quantity),2) AS 'Média'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o ON o.ProductID = p.ProductID
 GROUP BY Produto HAVING Média > 20
