1ecac78a6a69654b6a46d22b77544136c9baec4e
rogeriop1990cv <rogeriop1990cv@gmail.com>
2021-10-27 00:44:13 -0300

    Requisito 7 ok

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+    UCASE(concat(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`,
+    jhistory.START_DATE AS `Data de início`,
+    employees.SALARY AS `Salário`
+FROM
+    hr.job_history AS jhistory
+    INNER JOIN hr.employees AS employees ON employees.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
+WHERE
+    month(jhistory.START_DATE) <= 3
+ORDER BY
+    `Nome completo`;
