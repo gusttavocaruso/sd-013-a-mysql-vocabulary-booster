0ccda77711bff3d8e7a20414291fe319c23599a5
turtore <brunosap@gmail.com>
2021-10-27 11:04:58 -0300

    repara 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,18 +1,14 @@
 USE hr;
 DELIMITER $$;
 
-CREATE PROCEDURE buscar_media_por_cargo(IN nome_do_cargo VARCHAR(25), OUT media_salarial FLOAT)
+CREATE PROCEDURE buscar_media_por_cargo(IN nome_do_cargo VARCHAR(25))
 
 BEGIN
 	SELECT ROUND(AVG(salary), 2) AS 'Média salarial' FROM employees AS e
     INNER JOIN jobs as j
     ON e.job_id = j.job_id
-    WHERE job_title = nome_do_cargo
-    INTO media_salarial;
-    
+    WHERE job_title LIKE nome_do_cargo
+    GROUP BY j.job_title;    
 END $$;
 
 DELIMITER ;
-
-CALL buscar_media_por_cargo('Programmer', @media_salarial);
-SELECT @media_salarial;
