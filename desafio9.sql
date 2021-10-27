0583381be277a463e9b2d1223a819c22f040c578
rogeriop1990cv <rogeriop1990cv@gmail.com>
2021-10-27 02:31:54 -0300

    Requisito 9 ok

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT
+    concat(employees.FirstName, ' ', employees.LastName) AS `Nome completo`,
+    COUNT(*) AS `Total de pedidos`
+FROM
+    w3schools.orders AS `orders`
+    INNER JOIN w3schools.employees AS `employees` ON `orders`.EmployeeID = `employees`.EmployeeID
+GROUP BY
+    `orders`.EmployeeID
+ORDER BY
+    `Total de pedidos`;
