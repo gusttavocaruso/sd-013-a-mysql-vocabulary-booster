87be89e906ae7c99d84acddd8dda8e9c62f9ddf6
rogeriop1990cv <rogeriop1990cv@gmail.com>
2021-10-27 02:40:24 -0300

    Refatorando requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -6,6 +6,6 @@ FROM
     hr.job_history AS jhistory
     INNER JOIN hr.employees AS employees ON employees.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
 WHERE
-    month(jhistory.START_DATE) <= 3
+    month(jhistory.START_DATE) <= 3 and month(jhistory.START_DATE) >= 1
 ORDER BY
-    `Nome completo`;
+    `Nome completo` ASC;
