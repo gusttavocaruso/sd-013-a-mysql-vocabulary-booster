ce3434b2237cc00232994206be36ff3b0975875b
marcos <marc0sdurval20@gmail.com>
2021-10-26 21:27:27 -0300

    desafio15fixfixfix

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN Med VARCHAR(45))
 BEGIN
 SELECT
-	(SELECT round(AVG(e.SALARY),2)  FROM employees e 
+	(SELECT round(AVG(e.SALARY),2) FROM employees e 
     WHERE e.JOB_ID = j.JOB_ID) as "Média salarial"
     FROM jobs j
     WHERE j.JOB_TITLE = Med;
