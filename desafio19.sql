564260255007b6fea9b67b598f4a3cd86d1008bf
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-27 10:10:26 -0300

    challenge 19

---
+++ desafio19.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE pessoas_contratadas INT;
+	SELECT COUNT(*)
+	FROM hr.employees
+	WHERE MONTH(DATE(HIRE_DATE)) = mes AND YEAR(DATE(HIRE_DATE)) = ano
+	INTO pessoas_contratadas;
+RETURN pessoas_contratadas;
+END $$
+
+DELIMITER ;
