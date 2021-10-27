34a687be4bd30c025c154089c54f5e47413fa21f
turtore <brunosap@gmail.com>
2021-10-27 11:00:17 -0300

    repara requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -7,9 +7,5 @@ WHERE c1.country = c2.Country AND c1.CustomerID != c2.CustomerID
 ) 
 AS 'Número de compatriotas'
 FROM w3schools.customers AS c1
-WHERE 
-(
-SELECT COUNT(country) FROM w3schools.customers
-WHERE c1.country = Country AND c1.CustomerID != CustomerID
-) > 0
+HAVING `Número de compatriotas` > 0
 ORDER BY c1.ContactName;
