213a7e6c02da7ad8fbad5a284d7c5e5169d69462
Cae <eduardokae01@hotmail.com>
2021-10-27 09:58:11 -0300

    refatora desafio 17 parte 3

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT * FROM w3schools.customers;
+
+SELECT T1.ContactName AS Nome,
+T1.Country AS País, (
+SELECT COUNT(Country) FROM w3schools.customers
+WHERE T1.Country = Country
+AND T1.ContactName <> ContactName
+) AS 'Número de compatriotas'
+FROM w3schools.customers as T1;
