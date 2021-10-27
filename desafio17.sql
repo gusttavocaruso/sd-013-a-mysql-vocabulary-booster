80f9029be61db763be6e202a7f7ede726be98393
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-26 22:27:49 -0300

    challenge 17 one more time

--- desafio17.sql
+++ desafio17.sql
@@ -1,19 +1,10 @@
 DELIMITER $$
 
 CREATE TRIGGER fillDate
-AFTER INSERT ON w3schools.orders
+BEFORE INSERT ON w3schools.orders
 FOR EACH ROW
 BEGIN
-UPDATE w3schools.orders
-SET
-    OrderDate = NEW.CURRENT_DATE()
-WHERE
-    (SELECT
-            OrderID
-        FROM
-            w3schools.orders
-        ORDER BY OrderID DESC
-        LIMIT 1);
+SET NEW.OrderDate = CURRENT_DATE();
 END $$
 
 DELIMITER ;
