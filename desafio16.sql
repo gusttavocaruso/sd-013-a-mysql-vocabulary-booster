b6c051661f98057f0bf3c93177699ca16edc94a8
Cae <eduardokae01@hotmail.com>
2021-10-26 22:23:41 -0300

    refatora desafio 16 de PROCEDURE para FUNCTION

--- desafio16.sql
+++ desafio16.sql
@@ -1,13 +1,17 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(45))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(45))
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE quantidade_total INT;
   SELECT COUNT(H.JOB_ID) AS total_empregos
   FROM hr.job_history AS H
   INNER JOIN hr.employees AS E
   ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
-  WHERE E.EMAIL LIKE email;
+  WHERE E.EMAIL LIKE email
+  INTO quantidade_total;
+  RETURN quantidade_total;
 END $$
 
 DELIMITER ;
