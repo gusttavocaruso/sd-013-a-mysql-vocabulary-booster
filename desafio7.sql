9a3fdf028a5cdaaec5661f4499eee549762ca3d5
Linux <ivan@pop-os.localdomain>
2021-10-26 23:00:51 -0300

    Requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,4 @@ e.SALARY AS 'Salário'
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE MONTH(jh.START_DATE) IN (01, 02, 03)
-ORDER BY `Nome completo` ASC, `Salário` ASC;
+ORDER BY `Nome completo`, Salário;
