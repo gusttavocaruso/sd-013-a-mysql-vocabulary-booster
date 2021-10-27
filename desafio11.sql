068ce70a36ffd506f2a21d5573fe5a5abd7a9ea7
Thiago <thiagoc.carboneri@gmail.com>
2021-10-26 23:23:53 -0300

    requisito 13

--- desafio11.sql
+++ desafio11.sql
@@ -4,7 +4,9 @@ SELECT c.ContactName AS Nome,
     SELECT COUNT(c1.Country)
     FROM customers c1
     WHERE c1.Country = c.Country
-      AND c.ContactName <> c1.ContactName
+      AND c.CustomerID <> c1.CustomerID
   ) 'Número de compatriotas'
 FROM customers c
+GROUP BY c.CustomerID
+HAVING `Número de compatriotas` > 0
 ORDER BY c.ContactName ASC;
