179287ddd70370b5c317082c805daa5ac027f39d
Linux <ivan@pop-os.localdomain>
2021-10-26 22:18:42 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-j.JOB_TITLE AS Cargo, 
+j.JOB_TITLE AS 'Cargo', 
 jh.START_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_NAME AS Departamento
+d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS jh 
 INNER JOIN hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
